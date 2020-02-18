5248b3ffbe7924c8b0a69a28312133b29d782a0b
sreejitkar <sreejit.kar@science.christuniversity.in>
2020-02-18 22:35:58 +0530

    18_02_20

--- signup.php
+++ signup.php
@@ -6,32 +6,34 @@
 	<meta name=”viewport” content=”width=device-width, initial-scale="1.0">
 </head>
 <body>
-	<div style="width: 100%; height: 100vh;">
-			<img src="logo123.png" style="width: 7%;">
-			<center>
+	<div class="bodydiv">
+		<img src="logo123.png" style="width: 7%;">
+		<center>
 			<div class="container">	
 				<div class="signup-panel">
 					<div class="signup-panel-design">
-						<img src="photo-1541701494587-cb58502866ab.jpeg" class="signup-panel-design-logo">
+<!-- 						<div style="display: flex;"> -->
+							<img src="photo-1541701494587-cb58502866ab.jpeg" class="signup-panel-design-logo" style="z-index: 10;">
+<!-- 						</div> -->
+							<div class="signup-panel-design-text"><h3 class="signup-panel-design-text-heading"><span style="color: rgb(0,168,246);">i</span>NDRA</h3><br><h3>energy at your fingertips.</h3></div>
 					</div>
 					<div class="signup-panel-form">
 						<form action="" method="post">
 
-				<h2 style="color: white; letter-spacing: 2px;">Sign up To <span style="color: rgb(0,168,246);">iNDRA</span></h2>
-				<br><input type="Text" name="Username" id="Uname_log" placeholder="Username" class="inputbody" required autofocus>
-				<input type="Text" name="passwd" placeholder="First Name" class="inputbody" autofocus required><br>
-				<input type="Text" name="passwd" placeholder="Last Name" class="inputbody" autofocus required><br>
-				<input type="email" name="passwd" placeholder="E-mail ID" class="inputbody" autofocus required><br>
-				<input type="password" name="passwd" placeholder="Password" class="inputbody" autofocus required><br>
-				<input type="password" name="passwd" placeholder=" Confirm Password" class="inputbody" autofocus required><br>
+							<!-- <h2 style="color: white; letter-spacing: 2px; font-size: 180%;">Sign up To <span style="color: rgb(0,168,246);">iNDRA</span></h2> -->
+							<img src="roland-larsson-m2De0QR_tWo-unsplash.jpg" id="profilepic">
+							<br><input type="Text" name="Username" id="Uname_log" placeholder="Username" class="inputbody" required autofocus>
+							<input type="Text" name="fname" placeholder="Full Name" class="inputbody" autofocus required><br>
+							<input type="email" name="email" placeholder="E-mail ID" class="inputbody" autofocus required><br>
+							<input type="password" name="passwd" placeholder="Password" class="inputbody" autofocus required><br>
+							<input type="password" name="cpasswd" placeholder=" Confirm Password" class="inputbody" autofocus required><br>
 				<!-- <div style="display: flex; justify-content: space-around;">
 					<div>
 						<input type="checkbox" name="rem1" value="Remember Me" class="check1">
 						<label>Remember Me</label>
 					</div>
 					<a href="#forgot" >Forgot Password?</a></div> -->
-					<input type="submit" name="login" value="LOGIN" class="submitbut">
-					<br><h5>OR</h5><br>
+					<br><br>
 					<a href="signup.php" class="register-button" style="text-decoration: none;" >		
 						<span></span>
 						<span></span>
@@ -41,20 +43,20 @@
 					</a>
 
 				</form>
-					</div>
-				</div>
 			</div>
-		</center>
-		<div class="video-container">
-			<div class="video-overlay">
-			</div>
-			<video loop muted autoplay >
-				<source src="video1.mp4" type="video/mp4">
-			</video>
 		</div>
 	</div>
+</center>
+<div class="video-container">
+	<div class="video-overlay">
+	</div>
+	<video loop muted autoplay >
+		<source src="video1.mp4" type="video/mp4">
+		</video>
+	</div>
+</div>
 
 
-	</body>
+</body>
 </body>
 </html>
